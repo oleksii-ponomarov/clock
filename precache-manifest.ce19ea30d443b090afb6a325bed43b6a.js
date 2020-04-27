ec53a47fc33122e490cce4672abec8278b1a4071
Oleksii Ponomarov <oleksii.ponomarov@okami.tech>
2020-04-27 21:58:40 +0300

    Updates

---
+++ precache-manifest.ce19ea30d443b090afb6a325bed43b6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c608de4cebb8d87cfcd9800897508406",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "da5033e1be35d662512f",
+    "url": "/clock/static/css/main.118bf023.chunk.css"
+  },
+  {
+    "revision": "e5119999ac195b8ded1d",
+    "url": "/clock/static/js/2.27788ff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clock/static/js/2.27788ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5033e1be35d662512f",
+    "url": "/clock/static/js/main.e894f6f4.chunk.js"
+  },
+  {
+    "revision": "31a58c57cd923e9e29ae",
+    "url": "/clock/static/js/runtime-main.785f29d9.js"
+  }
+]);
